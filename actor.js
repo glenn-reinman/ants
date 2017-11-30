7bb07ed72cd130dec5317766b66b6924124c6267
quentintruong <quentintruong@users.noreply.github.com>
2017-11-30 10:57:12 -0800

    Actors' inheritance setup

---
+++ actor.js
@@ -0,0 +1,101 @@
+// actor.js
+
+class Actor {
+  constructor() {
+
+  }
+}
+
+class Pebble extends Actor {
+  constructor() {
+
+  }
+}
+
+class EnergyHolder extends Actor {
+  constructor() {
+
+  }
+}
+
+class Food extends EnergyHolder {
+  constructor() {
+
+  }
+}
+
+class AntHill extends EnergyHolder {
+  constructor() {
+
+  }
+}
+
+class Pheromone extends EnergyHolder {
+  constructor() {
+
+  }
+}
+
+class TriggerableActor extends Actor {
+  constructor() {
+
+  }
+}
+
+class WaterPool extends TriggerableActor {
+  constructor() {
+
+  }
+}
+
+class Poison extends TriggerableActor {
+  constructor() {
+
+  }
+}
+
+class Insect extends EnergyHolder {
+  constructor() {
+
+  }
+}
+
+class Ant extends Insect {
+  constructor() {
+
+  }
+}
+
+class Grasshopper extends Insect {
+  constructor() {
+
+  }
+}
+
+class BabyGrasshopper extends Grasshopper {
+  constructor() {
+
+  }
+}
+
+class AdultGrasshopper extends Grasshopper {
+  constructor() {
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
