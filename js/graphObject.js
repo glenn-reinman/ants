af10ef1ee40de8d0de2655558f8b3e538590abdd
quentintruong <quentintruong@users.noreply.github.com>
2017-12-02 00:35:34 -0800

    graphObject

--- js/graphObject.js
+++ js/graphObject.js
@@ -5,7 +5,33 @@ class GraphObject {
         this.img = imgs[img];
         this.x = startX;
         this.y = startY;
+        this.destX = startX;
+        this.destY = startY;
         this.dir = startDir;
         this.depth = depth;
     }
+
+    getX(){
+        return this.x;
+    }
+
+    getY(){
+        return this.y;
+    }
+
+    moveTo(x, y){
+        this.destX = x;
+        this.destY = y;
+    }
+
+    getDirection(){
+        return this.dir;
+    }
+
+    setDirection(dir){
+        this.dir = dir;
+    }
+
+    //updateImg(){ // update img according to current direction
+    //}
 }
