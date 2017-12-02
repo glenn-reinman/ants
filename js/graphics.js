7c752486bc779047ef4c645268dd292a021b900a
quentintruong <quentintruong@users.noreply.github.com>
2017-12-02 01:54:57 -0800

    redo graphics for 3d array actors; studentworld

--- js/graphics.js
+++ js/graphics.js
@@ -1,5 +1,4 @@
 // graphics.js
-
 var canvas = document.getElementById('graphics');
 var sim = document.getElementById("sim");
 var ctx = canvas.getContext("2d");
@@ -8,71 +7,10 @@ var w = window.getComputedStyle(sim).width
 canvas.width = parseInt(w.substring(0, w.length - 2));
 canvas.height = canvas.width;
 
-
 var offset = canvas.width/64;
 
-
-function populateArray(){
-
-	var actors = [];
-	for(var i = 0; i < 64; i++){
-		for(var j = 0; j < 64; j++){
-			if(i == 0 || j == 0 || i == 63 || j == 63){
-				let tmp = new Pebble(actors, i, j);
-				actors.push(tmp);
-			}
-		}
-	}
-
-	let f = new Food(actors, 1, 1, 100);
-	actors.push(f);
-	let ah = new AntHill(actors, 1, 2, Colony.green, 'p1');
-	actors.push(ah);
-
-	let pb = new Pheromone(actors, 1, 3, Colony.blue, Pheromone.pnone);
-	actors.push(pb);
-	let pg = new Pheromone(actors, 2, 3, Colony.green, Pheromone.pnone);
-	actors.push(pg);
-	let pr = new Pheromone(actors, 3, 3, Colony.red, Pheromone.pnone);
-	actors.push(pr);
-	let py = new Pheromone(actors, 4, 3, Colony.yellow, Pheromone.pnone);
-	actors.push(py);
-
-	let wp = new WaterPool(actors, 1, 4);
-	actors.push(wp);
-	let po = new Poison(actors, 1, 5);
-	actors.push(po);
-
-	let anb = new Ant(actors, 1, 6, Colony.blue, 'p2', 'ant');
-	actors.push(anb);
-	let ang = new Ant(actors, 2, 6, Colony.green, 'p2', 'ant');
-	actors.push(ang);
-	let anr = new Ant(actors, 3, 6, Colony.red, 'p2', 'ant');
-	actors.push(anr);
-	let any = new Ant(actors, 4, 6, Colony.yellow, 'p2', 'ant');
-	actors.push(any);
-
-	let bg = new BabyGrasshopper(actors, 1, 7);
-	actors.push(bg);
-	let ag = new AdultGrasshopper(actors, 1, 8);
-	actors.push(ag);
-
-	return actors;
-}
-
 function start(){
-	draw(populateArray());
-}
-
-function draw(arr) {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	for(var i = 0; i < 64; i++){
-		for(var j = 0; j < 64; j++){
-			ctx.fillStyle = ((i+j)%2 == 0)? "#ffffff": "#dddddd";
-			ctx.fillRect(i*offset, j*offset, offset, offset);
-		}
-	}
-	for(var i = 0; i < arr.length; i++){
-		ctx.drawImage(arr[i].img, arr[i].x*offset, arr[i].y*offset, offset, offset);
-	}
+	var sw = new StudentWorld();
+	sw.init();
+	sw.draw();
 }
--- js/studentWorld.js
+++ js/studentWorld.js
@@ -1,25 +1,90 @@
 // studentWorld.js
 
+
 class StudentWorld extends GameWorld{
 	constructor(){
-		/*
-		StudentWorld(std::string assetDir);
+		super()
+		this.numAnts0 = 0;
+		this.numAnts1 = 0;
+		this.numAnts2 = 0;
+		this.numAnts3 = 0;
 
-		m_numAnts0;
-		m_numAnts1;
-		m_numAnts2;
-		m_numAnts3;
-		m_numColonies;
-		std::vector<Actor*> m_world[VIEW_WIDTH][VIEW_HEIGHT];
-		Compiler* m_compilerForEntrant0;
-		Compiler* m_compilerForEntrant1;
-		Compiler* m_compilerForEntrant2;
-		Compiler* m_compilerForEntrant3;
-		m_ticks;
-		*/
+		this.compiler0 = null;
+		this.compiler1 = null;
+		this.compiler2 = null;
+		this.compiler3 = null;
+
+		this.numColonies = 0;
+
+		this.ticks = 0;
+
+		this.world = new Array(); for (var x = 0; x < 64; x++){ this.world[x] = new Array(); for (var y = 0; y < 64; y++){this.world[x][y] = new Array();}} // Makes 3D array
 	}
 
 	init(){
+		for(var x = 0; x < 64; x++){
+            for(var y = 0; y < 64; y++){
+                if(x == 0 || y == 0 || x == 63 || y == 63){
+                    let tmp = new Pebble(this.world, x, y);
+                    this.world[x][y].push(tmp);
+				}
+			}
+		}
+
+		//temporary display all
+		let f = new Food(this.world, 1, 1, 100);
+		this.world[1][1].push(f);
+		let ah = new AntHill(this.world, 1, 2, Colony.green, 'p1');
+		this.world[1][2].push(ah);
+
+		let pb = new Pheromone(this.world, 1, 3, Colony.blue, Pheromone.pnone);
+		this.world[1][3].push(pb);
+		let pg = new Pheromone(this.world, 2, 3, Colony.green, Pheromone.pnone);
+		this.world[2][3].push(pg);
+		let pr = new Pheromone(this.world, 3, 3, Colony.red, Pheromone.pnone);
+		this.world[3][3].push(pr);
+		let py = new Pheromone(this.world, 4, 3, Colony.yellow, Pheromone.pnone);
+		this.world[4][3].push(py);
+
+		let wp = new WaterPool(this.world, 1, 4);
+		this.world[1][4].push(wp);
+		let po = new Poison(this.world, 1, 5);
+		this.world[1][5].push(po);
+
+		let anb = new Ant(this.world, 1, 6, Colony.blue, 'p2', 'ant');
+		this.world[1][6].push(anb);
+		let ang = new Ant(this.world, 2, 6, Colony.green, 'p2', 'ant');
+		this.world[2][6].push(ang);
+		let anr = new Ant(this.world, 3, 6, Colony.red, 'p2', 'ant');
+		this.world[3][6].push(anr);
+		let any = new Ant(this.world, 4, 6, Colony.yellow, 'p2', 'ant');
+		this.world[4][6].push(any);
+
+		let bg = new BabyGrasshopper(this.world, 1, 7);
+		this.world[1][7].push(bg);
+		let ag = new AdultGrasshopper(this.world, 1, 8);
+		this.world[1][8].push(ag);
+		//end temporary display all 
+
+	}
+
+	draw(){
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		for(var i = 0; i < 64; i++){
+			for(var j = 0; j < 64; j++){
+				ctx.fillStyle = ((i+j)%2 == 0)? "#ffffff": "#dddddd";
+				ctx.fillRect(i*offset, j*offset, offset, offset);
+			}
+		}
+
+		for(var x = 0; x < 64; x++){
+            for(var y = 0; y < 64; y++){
+                if (this.world[x][y].length != 0){// probably will eventually have to make this display a specific image, rather than just the first
+                	ctx.drawImage(this.world[x][y][0].img, this.world[x][y][0].x*offset, this.world[x][y][0].y*offset, offset, offset);
+                }
+			}
+		}
+
 	}
 
 	move(){
@@ -49,7 +114,7 @@ class StudentWorld extends GameWorld{
 	addPheromone(x, y, colony){
 	}
 
-	addAnt(x, y, colony, compiler comp){
+	addAnt(x, y, colony, compiler, comp){
 	}
 
 	addAdult(x, y){
