922cd8af048c6dba0dcd8d9c1c6bcc038ba240b1
Gopi Suresh <suresh.gopikrishnan@gmail.com>
2018-01-04 15:00:23 -0800

    Canvas resizing implemented

--- js/editor/setup.js
+++ js/editor/setup.js
@@ -1,7 +1,11 @@
 //This file contains initial scripts to setup the ants environment
 
 //IDE Setup
-document.getElementById("ide").innerHTML = dumbAntProg;
+if(document.cookie)
+    document.getElementById("ide").innerHTML = document.cookie.substring(5);
+else
+    document.getElementById("ide").innerHTML = dumbAntProg;
+
 var editor = ace.edit("ide");
 editor.setTheme("ace/theme/monokai");
 editor.getSession().setMode("ace/mode/bug");
@@ -46,3 +50,27 @@ function run(){
 		sw.draw();
 	}, 100);
 }
+
+
+//resize handler for canvas
+function onResize( element, callback ){
+  var elementHeight = element.clientHeight,
+      elementWidth = element.clientWidth;
+  setInterval(function(){
+      if( element.clientHeight !== elementHeight || element.clientWidth !== elementWidth ){
+        elementHeight = element.clientHeight;
+        elementWidth = element.clientWidth;
+        callback(elementHeight, elementWidth);
+      }
+  }, 50);
+}
+
+onResize( document.getElementById("sim"), function(h, w){ canvas.width = w; canvas.height = h; offset = canvas.width/64; sw.draw(); } );
+
+
+//cookies for refresh
+
+setInterval(function(){
+    document.cookie = "code=" + editor.getValue();
+}, 300);
+ 
